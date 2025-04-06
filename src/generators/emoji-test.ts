42f54db0d2e5bab6c11d69d2a6fe11271e542562
Lucas <lucasnrgaard@gmail.com>
2025-04-06 08:20:53 +0200

    feat: add emoji test loom

---
+++ src/generators/emoji-test.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { createLoom } from "../loom";
+
+const entry = z.object({
+  codePoints: z.array(z.string()),
+  status: z.union([
+    z.literal("component"),
+    z.literal("fully-qualified"),
+    z.literal("minimally-qualified"),
+    z.literal("unqualified"),
+  ]),
+  comment: z.string(),
+});
+
+const emojiTestInputSchema = z.object({
+  group: z.string(),
+  subgroups: z.array(z.object({
+    subgroup: z.string(),
+    entries: z.array(entry),
+  })),
+});
+
+const emojiTestOptionsSchema = z.object({
+  separator: z.string(),
+  commentPrefix: z.string(),
+  version: z.string(),
+});
+
+export const emojiTest = createLoom({
+  inputSchema: emojiTestInputSchema,
+  optionsSchema: emojiTestOptionsSchema,
+  predicate: (ctx) => {
+    return ctx.isVersionLowerThan("3.x");
+  },
+  template: (ctx, item) => {
+    let template = `group: ${item.group}\n`;
+
+    for (const subgroup of item.subgroups) {
+      template += `subgroup: ${subgroup.subgroup}\n`;
+
+      for (const entry of subgroup.entries) {
+        template += `${entry.codePoints.join(" ")} ${ctx.options.separator} ${entry.status} ${ctx.options.commentPrefix} ${entry.comment}\n`;
+      }
+    }
+
+    return template;
+  },
+});
