0c4b12014a851b62493459a0b981e77c0220b50d
Lucas <lucasnrgaard@gmail.com>
2025-04-06 13:50:15 +0200

    feat: add presets to zwj sequences

--- src/generators/zwj-sequences.ts
+++ src/generators/zwj-sequences.ts
@@ -21,4 +21,66 @@ export const zwjSequences = createLoom({
     return `${item.codePoints.join(" ")} ${ctx.options.separator} ${item.type} ${ctx.options.separator} ${item.description} ${ctx.options.commentPrefix} ${item.comment}`;
   },
   eof: true,
+  presets: {
+    familyCombinations: [
+      {
+        codePoints: ["1F468", "200D", "1F469", "200D", "1F467"],
+        type: "zwj_sequence",
+        description: "family: man, woman, girl",
+        comment: "basic family combination",
+      },
+      {
+        codePoints: ["1F469", "200D", "1F469", "200D", "1F466"],
+        type: "zwj_sequence",
+        description: "family: woman, woman, boy",
+        comment: "same-sex parent family",
+      },
+      {
+        codePoints: ["1F468", "200D", "1F468", "200D", "1F467", "200D", "1F466"],
+        type: "zwj_sequence",
+        description: "family: man, man, girl, boy",
+        comment: "extended family combination",
+      },
+    ],
+    professions: [
+      {
+        codePoints: ["1F469", "200D", "1F3EB"],
+        type: "zwj_sequence",
+        description: "woman teacher",
+        comment: "profession with female emoji",
+      },
+      {
+        codePoints: ["1F468", "200D", "1F373"],
+        type: "zwj_sequence",
+        description: "man cook",
+        comment: "profession with male emoji",
+      },
+      {
+        codePoints: ["1F469", "200D", "1F4BB"],
+        type: "zwj_sequence",
+        description: "woman technologist",
+        comment: "modern profession",
+      },
+    ],
+    couples: [
+      {
+        codePoints: ["1F469", "200D", "2764", "FE0F", "200D", "1F468"],
+        type: "zwj_sequence",
+        description: "couple with heart: woman, man",
+        comment: "romantic combination with heart",
+      },
+      {
+        codePoints: ["1F468", "200D", "2764", "FE0F", "200D", "1F468"],
+        type: "zwj_sequence",
+        description: "couple with heart: man, man",
+        comment: "same-sex couple with heart",
+      },
+      {
+        codePoints: ["1F469", "200D", "2764", "FE0F", "200D", "1F469"],
+        type: "zwj_sequence",
+        description: "couple with heart: woman, woman",
+        comment: "same-sex couple with heart",
+      },
+    ],
+  },
 });
