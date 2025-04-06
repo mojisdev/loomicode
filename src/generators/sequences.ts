a9e9b41b87dc2b3a37f9f6cc9505162e725916e2
Lucas <lucasnrgaard@gmail.com>
2025-04-06 11:34:43 +0200

    refactor: rename schemas for clarity in sequences generator

--- src/generators/sequences.ts
+++ src/generators/sequences.ts
@@ -1,22 +1,22 @@
 import { type } from "arktype";
 import { createLoom } from "../loom";
 
-const ark_sequencesInputSchema = type({
+const sequencesInputSchema = type({
   codePoints: "string[]",
   type: "string",
   description: "string",
   comment: "string",
 });
 
-const ark_sequencesOptionsSchema = type({
+const sequencesOptionsSchema = type({
   separator: "string",
   commentPrefix: "string",
   version: "string",
 });
 
 export const sequences = createLoom({
-  inputSchema: ark_sequencesInputSchema,
-  optionsSchema: ark_sequencesOptionsSchema,
+  inputSchema: sequencesInputSchema,
+  optionsSchema: sequencesOptionsSchema,
   template: (ctx, item) => {
     //              ^?
     return `${item.codePoints.join(" ")} ${ctx.options.separator} ${item.type} ${ctx.options.separator} ${item.description} ${ctx.options.commentPrefix} ${item.comment}`;
