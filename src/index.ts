0510ca55d948497a1b4ecf98304e76b5d54801e9
Lucas <lucasnrgaard@gmail.com>
2025-04-06 14:23:43 +0200

    fix: export variations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { emojiTest } from "./generators/emoji-test";
 export { sequences } from "./generators/sequences";
+export { variations } from "./generators/variations";
 export { zwjSequences } from "./generators/zwj-sequences";
 
 export { createLoom } from "./loom";
