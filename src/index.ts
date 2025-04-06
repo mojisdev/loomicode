cc5fede2e72356a4b6239d9a4d1d58f2c7541422
Lucas <lucasnrgaard@gmail.com>
2025-04-06 08:30:50 +0200

    refactor: move Loom types to a new file and update imports
    - Created a new `types.ts` file to define `LoomContext`, `LoomConfig`, and `LoomInstance` interfaces.
    - Updated `index.ts` and `loom.ts` to import types from the new file, improving organization and clarity.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ export { sequences } from "./generators/sequences";
 export { zwjSequences } from "./generators/zwj-sequences";
 
 export { createLoom } from "./loom";
-export type { LoomConfig, LoomContext, LoomInstance } from "./loom";
+export type {
+  LoomConfig,
+  LoomContext,
+  LoomContextHelperFn,
+  LoomContextHelpers,
+  LoomInstance,
+} from "./types";
